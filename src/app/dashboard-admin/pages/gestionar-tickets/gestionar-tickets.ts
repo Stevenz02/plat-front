9e17d5c3b183eaf8661047bf01561b1ec027a894
Stevenz02 <jssmorales51@gmail.com>
2025-08-27 22:30:00 -0500

    Creacion paquetes y clases
    Creacion paquetes y clases del front

---
+++ src/app/dashboard-admin/pages/gestionar-tickets/gestionar-tickets.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestionar-tickets',
+  imports: [],
+  templateUrl: './gestionar-tickets.html',
+  styleUrl: './gestionar-tickets.css'
+})
+export class GestionarTickets {
+
+}
---
+++ src/app/dashboard/pages/crear-ticket/crear-ticket.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-ticket',
+  imports: [],
+  templateUrl: './crear-ticket.html',
+  styleUrl: './crear-ticket.css'
+})
+export class CrearTicket {
+
+}
