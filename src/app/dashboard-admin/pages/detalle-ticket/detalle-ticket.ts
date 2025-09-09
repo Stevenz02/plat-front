ec8365ddcc1fab1577ce8ac3fd9a2d03c6dfddc9
Stevenz02 <jssmorales51@gmail.com>
2025-09-08 20:58:27 -0500

    Cambios estructura gestionar tickets
    Cambio de estructura y vistas gestionar tickets

---
+++ src/app/dashboard-admin/pages/detalle-ticket/detalle-ticket.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-ticket',
+  imports: [],
+  templateUrl: './detalle-ticket.html',
+  styleUrl: './detalle-ticket.css'
+})
+export class DetalleTicketComponent {
+
+}
