193442fb3220483d21ef054ecbe07195833617a7
Stevenz02 <jssmorales51@gmail.com>
2025-11-08 20:27:57 -0500

    Update environment.prod.ts
    Implementacion URL back en google cloud run y google cloud sql

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://plat-back.onrender.com'  // URL del backend en producción (cambiar cuando tengas la real)
+  apiUrl: 'https://plat-back-service-482219095785.us-central1.run.app/api' // URL NUEVA (GOOGLE CLOUD))
+  // apiUrl: 'https://plat-back.onrender.com'  // URL ANTIGUA (RENDER)
 };
